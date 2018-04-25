9c1360b5cf2383db50df4d2e4fb9068903e60ea3
Joffrey LAGUT <joffrey.lagut@gmail.com>
2018-04-25 05:47:49 +0200

    Refactor the code of the table component to make it a presentational component.

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,33 +1,25 @@
 import React, { Component } from "react";
 
-class Table extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Table = props => {
+  const content = props.data.map(e => e);
+  const header = content.shift();
 
-  render() {
-    const content = this.props.data.map(e => e);
-    const header = content.shift();
-
-    return (
-      <table className="table is-striped is-narrow is-hoverable">
-        <thead>
-          <tr>
-            {header.map((column, index) => (
-              <th key={`th${index}`}>{column}</th>
-            ))}
+  return (
+    <table className="table is-striped is-narrow is-hoverable">
+      <thead>
+        <tr>
+          {header.map((column, index) => <th key={`th${index}`}>{column}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {content.map((row, index) => (
+          <tr key={`tr${index}`}>
+            {row.map((column, index) => <td key={`td${index}`}>{column}</td>)}
           </tr>
-        </thead>
-        <tbody>
-          {content.map((row, index) => (
-            <tr key={`tr${index}`}>
-              {row.map((column, index) => <td key={`td${index}`}>{column}</td>)}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-}
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table;
